b3dd2f7ec768c5447b207d154cd8ffb966daf5a7
nirav <nrvshah06@gmail.com>
2018-11-11 15:15:06 -0500

    added Carousel

--- src/Containers/Pages/HomePage/HomePage.js
+++ src/Containers/Pages/HomePage/HomePage.js
@@ -1,12 +1,16 @@
 
 import React, { Component } from 'react';
-import { PageHeader, Image, Grid, Row, Col, FormControl, Button, Nav, NavItem } from 'react-bootstrap';
+import { PageHeader, Carousel, Grid, Row, Col, FormControl, Button, Nav, NavItem } from 'react-bootstrap';
 
 
 import NavigationBar from '../../../Components/NavigationBar';
 
 
-import home from '../../../Assets/images/homebackground.jpeg'
+import home1 from '../../../Assets/images/home1.jpeg'
+import home2 from '../../../Assets/images/home2.jpg'
+import home3 from '../../../Assets/images/home3.jpg'
+import home4 from '../../../Assets/images/home4.jpg'
+import home5 from '../../../Assets/images/home5.jpg'
 class HomePage extends Component {
   constructor(props) {
     super(props);
@@ -40,13 +44,38 @@ class HomePage extends Component {
 
         <Grid>
           <Row>
-            <Col>
-              <Image src={home} responsive />
+            <Col >
+
+              <Carousel>
+                <Carousel.Item>
+                  <img alt="900x500" src={home1} responsive />
+                  <Carousel.Caption>
+                    <h3>A CLEVER Person Solves a Problem. A WISE Person Avoids It!.</h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img alt="900x500" src={home3} responsive />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img alt="900x500" src={home2} responsive />
+                  <Carousel.Caption>
+                    <h3>When the heart is at ease, the body is healthy.</h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img alt="900x500" src={home4} responsive />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img alt="900x500" src={home5} responsive />
+                  <Carousel.Caption>
+                    <h3 style={{ color: 'black' }}>The foundation of success in life is good health.
+                     A person cannot accumulate a fortune very well when he is sick.
+                     </h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
 
 
-              <div class="carousel-caption">
-                <h1>A CLEVER Person Solves a Problem. A WISE Person Avoids It!.</h1>
-              </div>
             </Col>
           </Row>
 
@@ -79,76 +108,76 @@ class HomePage extends Component {
             </form>
           </Row>
           <Row>
-          <Col lg={10}>
-        <div
-          style={{
-            marginTop: '2em',
-            marginLeft: '50px'
-          }}>
-          <Button bsSize="large" style={{ marginRight: '1em' }} >Fever</Button>
-          <Button bsSize="large" style={{ marginRight: '1em' }} >Cough</Button>
-          <Button bsSize="large" style={{ marginRight: '1em' }} >Running Nose</Button>
-          <Button bsSize="large" style={{ marginRight: '1em' }} >Nausea</Button>
-        </div>
-        </Col>
-        <Col lg={10}>
-        <div
-          style={{
-            marginTop: '1.5em',
-            marginLeft: '50px'
-          }}>
-          <Button bsSize="large" style={{ marginRight: '1em' }} >Headace</Button>
-          <Button bsSize="large" style={{ marginRight: '1em' }} >Fatigue</Button>
-          <Button bsSize="large" style={{ marginRight: '1em' }} >Dizziness</Button>
-          <Button bsSize="large" style={{ marginRight: '1em' }} >Diarrhea</Button>
-        </div>
-        </Col>
-        </Row>
+            <Col lg={10}>
+              <div
+                style={{
+                  marginTop: '2em',
+                  marginLeft: '50px'
+                }}>
+                <Button bsSize="large" style={{ marginRight: '1em' }} >Fever</Button>
+                <Button bsSize="large" style={{ marginRight: '1em' }} >Cough</Button>
+                <Button bsSize="large" style={{ marginRight: '1em' }} >Running Nose</Button>
+                <Button bsSize="large" style={{ marginRight: '1em' }} >Nausea</Button>
+              </div>
+            </Col>
+            <Col lg={10}>
+              <div
+                style={{
+                  marginTop: '1.5em',
+                  marginLeft: '50px'
+                }}>
+                <Button bsSize="large" style={{ marginRight: '1em' }} >Headace</Button>
+                <Button bsSize="large" style={{ marginRight: '1em' }} >Fatigue</Button>
+                <Button bsSize="large" style={{ marginRight: '1em' }} >Dizziness</Button>
+                <Button bsSize="large" style={{ marginRight: '1em' }} >Diarrhea</Button>
+              </div>
+            </Col>
+          </Row>
         </Grid>
 
-<div
+        <div
           style={{
             backgroundColor: '#f8f8f8',
             marginTop: '1.5em',
           }}>
 
 
-        <Grid style={{ padding: '5em 0em' }}>
+          <Grid style={{ padding: '5em 0em' }}>
 
 
-          <Row>
-            <Col md="2">
-              <h5 >About</h5>
-              <Nav bsStyle="pills" stacked  >
-                <NavItem eventKey={1} href="/contact">
-                  Site Map
+            <Row>
+              <Col md="2">
+                <h5 >About</h5>
+                <Nav bsStyle="pills" stacked  >
+                  <NavItem eventKey={1} href="/contact">
+                    Site Map
     </NavItem>
-                <NavItem eventKey={2} href="/faq">
-                  Contact Us
+                  <NavItem eventKey={2} href="/faq">
+                    Contact Us
     </NavItem>
-                <NavItem eventKey={3} href="/faq">
-                  How To Register
+                  <NavItem eventKey={3} href="/faq">
+                    How To Register
     </NavItem>
-              </Nav>
-            </Col>
+                </Nav>
+              </Col>
 
 
-            <Col md="2">
-              <h5 >Diagnosis</h5>
-              <Nav bsStyle="pills" stacked  >
-                <NavItem eventKey={1} href="/home">
-                  Get Started
+              <Col md="2">
+                <h5 >Diagnosis</h5>
+                <Nav bsStyle="pills" stacked  >
+                  <NavItem eventKey={1} href="/home">
+                    Get Started
     </NavItem>
-                <NavItem eventKey={2} href="/faq">
-                  FAQ
+                  <NavItem eventKey={2} href="/faq">
+                    FAQ
     </NavItem>
-                <NavItem eventKey={3} href="/faq">
-                  How To Use
+                  <NavItem eventKey={3} href="/faq">
+                    How To Use
     </NavItem>
-              </Nav>
-            </Col>
-          </Row>
-        </Grid>
+                </Nav>
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </div>
     )
