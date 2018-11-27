ae31d7867ccdd0efe892eaa8846fe581ee0b1ad5
nirav <nrvshah06@gmail.com>
2018-11-27 17:15:00 -0500

    still working on FAQ page

--- app/src/Containers/Pages/FaqPage/FaqPage.js
+++ app/src/Containers/Pages/FaqPage/FaqPage.js
@@ -79,9 +79,9 @@ class FaqPage extends Component {
        <div className="navbar-fixed-top"style= {{ top: '220px' }}>
 	<ul className="cd-faq-categories" >
 		<li><a className={this.state.selected==='basics'? 'selected' :''} onClick={() => this.changeSelected('basics')} href="#basics">Basics</a></li>
-		<li><a className={this.state.selected==='mobile'? 'selected' :'' } onClick={() => this.changeSelected('mobile')} href="#mobile">Mobile</a></li>
-		<li><a className={this.state.selected==='account'? 'selected' :''} onClick={() => this.changeSelected('account')} href="#account">Account</a></li>
-		<li><a className={this.state.selected==='payments'? 'selected' :'' } onClick={() => this.changeSelected('payments')} href="#payments">Payments</a></li>
+		<li><a className={this.state.selected==='mobile'? 'selected' :'' } onClick={() => this.changeSelected('mobile')} href="#mobile">Daignosis</a></li>
+		<li><a className={this.state.selected==='account'? 'selected' :''} onClick={() => this.changeSelected('account')} href="#account">Contribute</a></li>
+		
 	
     </ul> 
 </div>
@@ -89,9 +89,9 @@ class FaqPage extends Component {
 		<ul id="basics" className="cd-faq-group">
 			<li className="cd-faq-title"><h2>Basics</h2></li>
 			<li className={this.state.show1===true? 'content-visible' :''}>
-				<a name='show1' className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I change my password?</a>
+				<a name='show1' className="cd-faq-trigger" href="#0" onClick={this.showContent}>How can I log in?</a>
 				{this.state.show1 && <div className="cd-faq-content::after">
-					<p  style={customStyles}> go here and do
+					<p  style={customStyles}> go to nav bar and click on log in button
                     </p></div>
                 }
 			</li>
@@ -99,14 +99,15 @@ class FaqPage extends Component {
 			<li className={this.state.show2===true? 'content-visible' :''}>
 				<a name='show2' className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I sign up?</a>
 				{this.state.show2 &&<div className="cd-faq-content::after">
-					<p  style= {{ 'line-height': '1.6' }}> sign up here</p></div>
+					<p  style= {{ 'line-height': '1.6' }}>  go to nav bar and click on Sign in button</p></div>
                 }
 			</li>
 
 			<li className={this.state.show3===true? 'content-visible' :''}>
-				<a name="show3"className="cd-faq-trigger" href="#0" onClick={this.showContent}>Can I remove a post?</a>
+				<a name="show3"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How can you conact us?</a>
 				{this.state.show3  && <div className="cd-faq-content::after">
-					<p  style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
+					<p  style={customStyles}>
+					go to nav bar and click on Contact button</p>
 				</div>
                 }
 			</li>
@@ -121,9 +122,9 @@ class FaqPage extends Component {
 		</ul> 
 
 		<ul id="mobile" className="cd-faq-group">
-			<li className="cd-faq-title"><h2>Mobile</h2></li>
+			<li className="cd-faq-title"><h2>Daignosis</h2></li>
 			<li className={this.state.show5===true? 'content-visible' :''}>
-				<a name="show5"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How does syncing work?</a>
+				<a name="show5"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How does prediction work?</a>
 				{this.state.show5 && <div className="cd-faq-content::after">
 					<p  style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.</p>
 				</div>
@@ -131,7 +132,7 @@ class FaqPage extends Component {
 			</li>
 
 			<li className={this.state.show6===true? 'content-visible' :''}>
-				<a name="show6"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I upload files from my phone or tablet?</a>
+				<a name="show6"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How can I enter my daily habits like smoking in the system?</a>
 				{this.state.show6 &&<div className="cd-faq-content::after">
 					<p  style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempore, placeat quisquam rerum! Eligendi fugit dolorum tenetur modi fuga nisi rerum, autem officiis quaerat quos. Magni quia, quo quibusdam odio. Error magni aperiam amet architecto adipisci aspernatur! Officia, quaerat magni architecto nostrum magnam fuga nihil, ipsum laboriosam similique voluptatibus facilis nobis? Eius non asperiores, nesciunt suscipit veniam blanditiis veritatis provident possimus iusto voluptas, eveniet architecto quidem quos molestias, aperiam eum reprehenderit dolores ad deserunt eos amet. Vero molestiae commodi unde dolor dicta maxime alias, velit, nesciunt cum dolorem, ipsam soluta sint suscipit maiores mollitia assumenda ducimus aperiam neque enim! Quas culpa dolorum ipsam? Ipsum voluptatibus numquam natus? Eligendi explicabo eos, perferendis voluptatibus hic sed ipsam rerum maiores officia! Beatae, molestias!</p>
 				</div>
@@ -139,7 +140,7 @@ class FaqPage extends Component {
 			</li>
 
 			<li className={this.state.show7===true? 'content-visible' :''}>
-				<a name="show7"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I link to a file or folder?</a>
+				<a name="show7"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I find remedies for the illness?</a>
 				{this.state.show7 && <div className="cd-faq-content::after">
 					<p  style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
 				</div>
