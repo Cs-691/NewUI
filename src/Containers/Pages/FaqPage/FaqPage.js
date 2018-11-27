706fa4bdb63fca7180b5c27b5ce6f812d51cb6ed
nirav <nrvshah06@gmail.com>
2018-11-27 00:59:27 -0500

    fix style again

--- src/Containers/Pages/FaqPage/FaqPage.js
+++ src/Containers/Pages/FaqPage/FaqPage.js
@@ -53,7 +53,18 @@ class FaqPage extends Component {
   render() {
 
 
-
+	const customStyles = {
+		
+			
+			'font-size': '1.4rem',
+			'line-height': '1.6',
+			'color': '#6c7d8e',
+			'margin-top':'25px',
+			'text-align': 'justify',
+   
+			
+		
+	};
     return (
 
       <div id="faqPage">
@@ -80,7 +91,7 @@ class FaqPage extends Component {
 			<li className={this.state.show1===true? 'content-visible' :''}>
 				<a name='show1' className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I change my password?</a>
 				{this.state.show1 && <div className="cd-faq-content::after">
-					<p> go here and do
+					<p  style={customStyles}> go here and do
                     </p></div>
                 }
 			</li>
@@ -88,14 +99,14 @@ class FaqPage extends Component {
 			<li className={this.state.show2===true? 'content-visible' :''}>
 				<a name='show2' className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I sign up?</a>
 				{this.state.show2 &&<div className="cd-faq-content::after">
-					<p> sign up here</p></div>
+					<p  style= {{ 'line-height': '1.6' }}> sign up here</p></div>
                 }
 			</li>
 
 			<li className={this.state.show3===true? 'content-visible' :''}>
 				<a name="show3"className="cd-faq-trigger" href="#0" onClick={this.showContent}>Can I remove a post?</a>
 				{this.state.show3  && <div className="cd-faq-content::after">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
+					<p  style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
 				</div>
                 }
 			</li>
@@ -103,7 +114,7 @@ class FaqPage extends Component {
 			<li className={this.state.show4===true? 'content-visible' :''}>
 				<a name="show4"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do reviews work?</a>
 				{this.state.show4 &&<div className="cd-faq-content::after">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
+					<p style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
 				</div>
                 }
 			</li>
@@ -114,7 +125,7 @@ class FaqPage extends Component {
 			<li className={this.state.show5===true? 'content-visible' :''}>
 				<a name="show5"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How does syncing work?</a>
 				{this.state.show5 && <div className="cd-faq-content::after">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.</p>
+					<p  style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.</p>
 				</div>
                 }
 			</li>
@@ -122,7 +133,7 @@ class FaqPage extends Component {
 			<li className={this.state.show6===true? 'content-visible' :''}>
 				<a name="show6"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I upload files from my phone or tablet?</a>
 				{this.state.show6 &&<div className="cd-faq-content::after">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempore, placeat quisquam rerum! Eligendi fugit dolorum tenetur modi fuga nisi rerum, autem officiis quaerat quos. Magni quia, quo quibusdam odio. Error magni aperiam amet architecto adipisci aspernatur! Officia, quaerat magni architecto nostrum magnam fuga nihil, ipsum laboriosam similique voluptatibus facilis nobis? Eius non asperiores, nesciunt suscipit veniam blanditiis veritatis provident possimus iusto voluptas, eveniet architecto quidem quos molestias, aperiam eum reprehenderit dolores ad deserunt eos amet. Vero molestiae commodi unde dolor dicta maxime alias, velit, nesciunt cum dolorem, ipsam soluta sint suscipit maiores mollitia assumenda ducimus aperiam neque enim! Quas culpa dolorum ipsam? Ipsum voluptatibus numquam natus? Eligendi explicabo eos, perferendis voluptatibus hic sed ipsam rerum maiores officia! Beatae, molestias!</p>
+					<p  style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempore, placeat quisquam rerum! Eligendi fugit dolorum tenetur modi fuga nisi rerum, autem officiis quaerat quos. Magni quia, quo quibusdam odio. Error magni aperiam amet architecto adipisci aspernatur! Officia, quaerat magni architecto nostrum magnam fuga nihil, ipsum laboriosam similique voluptatibus facilis nobis? Eius non asperiores, nesciunt suscipit veniam blanditiis veritatis provident possimus iusto voluptas, eveniet architecto quidem quos molestias, aperiam eum reprehenderit dolores ad deserunt eos amet. Vero molestiae commodi unde dolor dicta maxime alias, velit, nesciunt cum dolorem, ipsam soluta sint suscipit maiores mollitia assumenda ducimus aperiam neque enim! Quas culpa dolorum ipsam? Ipsum voluptatibus numquam natus? Eligendi explicabo eos, perferendis voluptatibus hic sed ipsam rerum maiores officia! Beatae, molestias!</p>
 				</div>
                 }
 			</li>
@@ -130,7 +141,7 @@ class FaqPage extends Component {
 			<li className={this.state.show7===true? 'content-visible' :''}>
 				<a name="show7"className="cd-faq-trigger" href="#0" onClick={this.showContent}>How do I link to a file or folder?</a>
 				{this.state.show7 && <div className="cd-faq-content::after">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
+					<p  style={customStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
 				</div>
                 }
 			</li>
